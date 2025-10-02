c73df4a9c76e61f81a3478c6f6eb1759e7f90312
lalithkumar24 <lali2204.it@rmkec.ac.in>
2025-10-02 22:42:38 +0530

    Added c/c++,python debugging support

--- lua/utils/lsp.lua
+++ lua/utils/lsp.lua
@@ -8,9 +8,9 @@ M.on_attach = function(event)
 	local bufnr = event.buf
 	local keymap = vim.keymap.set
 	local opts = {
-		noremap = true, -- prevent recursive mapping
-		silent = true, -- don't print the command to the cli
-		buffer = bufnr, -- restrict the keymap to the local buffer number
+		noremap = true,
+		silent = true,
+		buffer = bufnr,
 	}
 
 	-- native neovim keymaps
@@ -34,6 +34,25 @@ M.on_attach = function(event)
 
 	-- Order Imports (if supported by the client LSP)
 	if client:supports_method("textDocument/codeAction", bufnr) then
+	-- === LSP keymaps ===
+	keymap("n", "<leader>gD", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
+	keymap("n", "<leader>gS", "<cmd>vsplit | lua vim.lsp.buf.definition()<CR>", opts)
+	keymap("n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
+	keymap("n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
+	keymap("n", "<leader>D", "<cmd>lua vim.diagnostic.open_float({ scope = 'line' })<CR>", opts)
+	keymap("n", "<leader>d", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
+	keymap("n", "<leader>pd", "<cmd>lua vim.diagnostic.goto_prev()<CR>", opts)
+	keymap("n", "<leader>nd", "<cmd>lua vim.diagnostic.goto_next()<CR>", opts)
+	keymap("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
+	-- === fzf-lua keymaps ===
+	keymap("n", "<leader>gd", "<cmd>FzfLua lsp_finder<CR>", opts)
+	keymap("n", "<leader>gr", "<cmd>FzfLua lsp_references<CR>", opts)
+	keymap("n", "<leader>gt", "<cmd>FzfLua lsp_typedefs<CR>", opts)
+	keymap("n", "<leader>ds", "<cmd>FzfLua lsp_document_symbols<CR>", opts)
+	keymap("n", "<leader>ws", "<cmd>FzfLua lsp_workspace_symbols<CR>", opts)
+	keymap("n", "<leader>gi", "<cmd>FzfLua lsp_implementations<CR>", opts)
+	-- === Order Imports ===
+	if client.supports_method("textDocument/codeAction") then
 		keymap("n", "<leader>oi", function()
 			vim.lsp.buf.code_action({
 				context = {
@@ -43,22 +62,39 @@ M.on_attach = function(event)
 				apply = true,
 				bufnr = bufnr,
 			})
-			-- format after changing import order
 			vim.defer_fn(function()
 				vim.lsp.buf.format({ bufnr = bufnr })
-			end, 50) -- slight delay to allow for the import order to go first
+			end, 50)
 		end, opts)
 	end
-
 	-- === DAP keymaps ===
-	if client.name == "rust-analyzer" then -- debugging only configured for Rust
+	-- Languages with debugging configured: Python, C/C++, Rust, Zig, JavaScript, TypeScript
+	local debug_clients = {
+		"pylsp", "pyright", "basedpyright",  -- Python
+		"clangd", "ccls",                     -- C/C++
+		"rust_analyzer",                      -- Rust (note: underscore not dash)
+		"zls",                                -- Zig
+		"tsserver", "vtsls", "eslint",       -- JavaScript/TypeScript
+	}
+	local supports_debugging = false
+	for _, lang in ipairs(debug_clients) do
+		if client.name == lang then
+			supports_debugging = true
+			break
+		end
+	end
+	if supports_debugging then
 		local dap = require("dap")
-		keymap("n", "<leader>dc", dap.continue, opts) -- Continue / Start
-		keymap("n", "<leader>do", dap.step_over, opts) -- Step over
-		keymap("n", "<leader>di", dap.step_into, opts) -- Step into
-		keymap("n", "<leader>du", dap.step_out, opts) -- Step out
-		keymap("n", "<leader>db", dap.toggle_breakpoint, opts) -- Toggle breakpoint
-		keymap("n", "<leader>dr", dap.repl.open, opts) -- Open DAP REPL
+		keymap("n", "<leader>dc", dap.continue, opts)           -- Start/Continue
+		keymap("n", "<leader>do", dap.step_over, opts)          -- Step over
+		keymap("n", "<leader>di", dap.step_into, opts)          -- Step into
+		keymap("n", "<leader>du", dap.step_out, opts)           -- Step out
+		keymap("n", "<leader>db", dap.toggle_breakpoint, opts)  -- Toggle breakpoint
+		keymap("n", "<leader>dB", function()                    -- Conditional breakpoint
+			dap.set_breakpoint(vim.fn.input("Breakpoint condition: "))
+		end, opts)
+		keymap("n", "<leader>dr", dap.repl.open, opts)          -- Open REPL
+		keymap("n", "<leader>dt", dap.terminate, opts)          -- Terminate session
 	end
 end
 
